da8d0197fdb977114a4e4e6f34eb10213651d4a2
pyama86 <www.kazu.com@gmail.com>
2019-06-05 11:31:24 +0900

    Brew formula update for cluster-caution version 0.1.0

--- cluster-caution.rb
+++ cluster-caution.rb
@@ -4,13 +4,13 @@ class ClusterCaution < Formula
   homepage "https://github.com/pyama86/cluster-caution"
   url "https://github.com/pyama86/cluster-caution/releases/download/0.1.0/cluster-caution_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "7cee1418d30d2ffe360e11709dacfcb1e2cde2387ac0741e9a461232cbce3c93"
+  sha256 "a873b59fce14811b2d5a8560d0df862c15d925dd7332a40a5ba9fc556727930f"
 
   def install
-    bin.install 'kubectl-caution'
+    bin.install 'kubectl-cluster-caution'
   end
 
   test do
-    system "#{bin}/kubectl-caution"
+    system "#{bin}/kubectl-cluster-caution"
   end
 end
