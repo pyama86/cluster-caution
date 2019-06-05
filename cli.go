307e7cd55aa54f0e03a4c089b75ead5740222805
pyama86 <pyama@pepabo.com>
2019-06-05 10:25:18 +0900

    perfect!

--- cli.go
+++ cli.go
@@ -9,7 +9,10 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"syscall"
 
+	"github.com/Songmu/prompter"
+	"github.com/k0kubun/pp"
 	flag "github.com/spf13/pflag"
 
 	"k8s.io/cli-runtime/pkg/genericclioptions"
@@ -44,14 +47,16 @@ func homeDir() string {
 // Run invokes the CLI with the given arguments.
 func (cli *CLI) Run(args []string) int {
 	if err := cli.run(args); err != nil {
-		fmt.Errorf("%s\n", err)
+		fmt.Println(err)
 		return ExitCodeError
 	}
 	return ExitCodeOK
 }
+
 func (cli *CLI) run(args []string) error {
 	var (
-		init bool
+		add bool
+		del bool
 
 		version bool
 	)
@@ -59,8 +64,8 @@ func (cli *CLI) run(args []string) error {
 	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
 	flags.SetOutput(cli.errStream)
 
-	flags.BoolVar(&init, "init", false, "Init Repository")
-	flags.BoolVar(&init, "i", false, "Init Repository(Short)")
+	flags.BoolVar(&add, "add-context", false, "Add Context Config to Repository")
+	flags.BoolVar(&del, "delete-context", false, "Delete Context Config to Repository")
 
 	flags.BoolVar(&version, "version", false, "Print version information and quit.")
 	cli.configFlags.AddFlags(flags)
@@ -84,22 +89,72 @@ func (cli *CLI) run(args []string) error {
 	}
 
 	fp := filepath.Join(strings.TrimRight(top, "\n"), ".kube-cluster-coution")
+
+	acs, err := readFile(fp)
+	if err != nil {
+		return err
+	}
+
 	// Show version
 	if version {
 		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
 		return nil
 	}
 
-	if init {
-		if err := writeFile(currentContext, fp); err != nil {
-			return err
+	resultMessage := fmt.Sprintf("add cluster %s(%s)\n", currentContext.Cluster, currentContext.Namespace)
+	if add {
+		a, exist := uniqueAppendContext(acs, currentContext)
+		if exist {
+			resultMessage = fmt.Sprintf("cluster %s(%s) is exists\n", currentContext.Cluster, currentContext.Namespace)
 		}
+		acs = a
+	} else if del {
+		a, exist := deleteContext(acs, currentContext)
+		if exist {
+			resultMessage = fmt.Sprintf("delete cluster %s(%s)\n", currentContext.Cluster, currentContext.Namespace)
+		} else {
+			resultMessage = fmt.Sprintf("cluster %s(%s) is not exists\n", currentContext.Cluster, currentContext.Namespace)
+		}
+		acs = a
+	} else {
+		runKubectl(acs, currentContext)
 	}
 
+	if add || del {
+		if err := writeFile(acs, fp); err != nil {
+			return err
+		}
+		fmt.Println(resultMessage)
+	}
 	return nil
 }
 
-func writeFile(ac *api.Context, path string) error {
+func uniqueAppendContext(ac []*api.Context, newAc *api.Context) ([]*api.Context, bool) {
+	exist := false
+	for _, a := range ac {
+		if a.Cluster == newAc.Cluster && a.Namespace == newAc.Namespace {
+			exist = true
+		}
+	}
+	if !exist {
+		ac = append(ac, newAc)
+	}
+	return ac, exist
+}
+func deleteContext(ac []*api.Context, delAc *api.Context) ([]*api.Context, bool) {
+	ret := []*api.Context{}
+	exist := false
+	for _, a := range ac {
+		if a.Cluster == delAc.Cluster && a.Namespace == delAc.Namespace {
+			exist = true
+			continue
+		}
+		ret = append(ret, delAc)
+	}
+	return ret, exist
+}
+
+func writeFile(ac []*api.Context, path string) error {
 	bdata, err := json.Marshal(ac)
 	if err != nil {
 		return err
@@ -107,17 +162,52 @@ func writeFile(ac *api.Context, path string) error {
 
 	return ioutil.WriteFile(path, bdata, os.ModePerm)
 }
-func readFile(path string) (*api.Context, error) {
+
+func readFile(path string) ([]*api.Context, error) {
 	raw, err := ioutil.ReadFile(path)
 	if err != nil {
+		if _, notfound := err.(*os.PathError); notfound {
+			return []*api.Context{}, nil
+		}
+		pp.Println(err)
 		return nil, err
 	}
-	var ac api.Context
+	var ac []*api.Context
 	err = json.Unmarshal(raw, &ac)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("can't parse .kube-cluster-coution %s", err.Error())
 	}
-	return &ac, nil
+	return ac, nil
+}
+
+func runKubectl(ac []*api.Context, cur *api.Context) {
+	exist := false
+	for _, a := range ac {
+		if a.Cluster == cur.Cluster && a.Namespace == cur.Namespace {
+			exist = true
+		}
+	}
+
+	if !exist {
+		if !prompter.YesNo("Repository configuration is different from cluster or namespace.\nDo you want to continue?(Y/n)", true) {
+			return
+		}
+	}
+	args := os.Args[1:]
+	cmd := exec.Command("kubectl", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		status := 1
+		if e2, ok := err.(*exec.ExitError); ok {
+			if s, ok := e2.Sys().(syscall.WaitStatus); ok {
+				status = s.ExitStatus()
+			}
+		}
+		os.Exit(status)
+	}
 }
 
 func getRepositoryTop() (string, error) {
